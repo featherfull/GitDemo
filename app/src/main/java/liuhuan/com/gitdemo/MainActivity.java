599e05296f5fe65beca1a5b1ead126839e50eebe
liy <liy@riwise.cn>
2018-10-11 14:44:53 +0800

    dada

--- app/src/main/java/liuhuan/com/gitdemo/MainActivity.java
+++ app/src/main/java/liuhuan/com/gitdemo/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text="刘欢";
     }
 }
