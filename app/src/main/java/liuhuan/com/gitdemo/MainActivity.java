f1e37e0cb9836a10b1ea885c53809584ed371b72
lhh <850151071@qq.com>
2018-10-11 16:58:36 +0800

    lhh提交修改代码

--- app/src/main/java/liuhuan/com/gitdemo/MainActivity.java
+++ app/src/main/java/liuhuan/com/gitdemo/MainActivity.java
@@ -2,14 +2,24 @@ package liuhuan.com.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private String text;
+    private TextView next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text="刘欢";
+        next=(TextView)findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewActivity.open(MainActivity.this,text);
+            }
+        });
     }
 }
--- app/src/main/java/liuhuan/com/gitdemo/NewActivity.java
+++ app/src/main/java/liuhuan/com/gitdemo/NewActivity.java
@@ -1,8 +1,33 @@
 package liuhuan.com.gitdemo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by ahrz7 on 2018/10/11.
+ * lhh修改的
  */
 
-public class NewActivity {
+public class NewActivity extends AppCompatActivity {
+    private TextView next;
+
+    public static void open(Context context,String text){
+        Intent intent=new Intent(context,NewActivity.class);
+        intent.putExtra("text",text);
+        context.startActivity(intent);
+    }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_layout);
+        next=(TextView)findViewById(R.id.next);
+        if(getIntent().getStringExtra("text")!=null) {
+            next.setText(getIntent().getStringExtra("text"));
+        }
+
+    }
 }
